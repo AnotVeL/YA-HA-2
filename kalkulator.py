dc0f0906099d9825f937bc7a0a348397e357214b
AnotVeL <158256958+AnotVeL@users.noreply.github.com>
2024-02-07 14:00:34 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -4,16 +4,17 @@ import streamlit as st
 st.header('Wira :sparkles:')
 st.subheader('Calculator')
 
-c1, c2 = st.columns(2)
+c1, c2, c3 = st.columns(3)
 
 with c1:
-  x = st.number_input('Suhu ',value=100)
+  x = st.number_input('Number ',value=100)
   st.write('=>: ')
 with c2:
   satuan = st.selectbox(
-    'Satuan',
+    'Operator',
     ('+', '-', 'x', ':'),key='k1')
   st.write(':sparkles:')
+  
 st.write(x,'',satuan,' = ',' ')
 
 st.caption('Copyright @ AVL Corp. 2024')
