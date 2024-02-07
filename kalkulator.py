7ca68a7eb5a7e92d576fd1280f891e7cbf8bfcc8
AnotVeL <158256958+AnotVeL@users.noreply.github.com>
2024-02-07 13:47:33 +0700

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Header
+st.header('Wira :sparkles:')
+st.subheader('Plot')
+
+c1, c2 = st.columns(2)
+
+with c1:
+  x = st.number_input('Suhu ',value=100)
+  st.write('=>: ')
+with c2:
+  satuan = st.selectbox(
+    'Satuan',
+    ('+', '-', 'x', ':'),key='k1')
+    st.write(':sparkles: ')
+st.write(x,'',satuan,' = ',' ')
+
+st.caption(Copyright)
